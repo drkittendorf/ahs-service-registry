efac2343db1d84fd218c813653b1e660fb19fd54
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-23 13:25:45 -0400

    added initialze files

---
+++ src/main/java/com/adhocsensei/ahsserviceregistry/AhsServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsServiceRegistryApplication.class, args);
+	}
+
+}
